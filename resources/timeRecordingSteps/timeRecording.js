c69196a6173e3cea33581ba38ba3a1972d82eb7f
Renan Silveira <renan.silveira@globalhitss.com.br>
2019-05-08 09:55:58 -0300

    Confirmar validacao de folha de ponto no modal

--- resources/timeRecordingSteps/timeRecording.js
+++ resources/timeRecordingSteps/timeRecording.js
@@ -1,15 +1,37 @@
-require('dotenv').config();
 const moment = require('moment');
 const { selectors, screenshot, consoleLog } = require('../../helpers/');
 
-module.exports = async puppet => {
-  const page = puppet.page;
-  await page.goto(process.env.URL_TIMERECORDING, { waitUntil: 'networkidle0' });
+const timeRecording = async({ page }) => {
+  if (page.url() !== process.env.URL_TIMERECORDING) {
+    await page.goto(process.env.URL_TIMERECORDING, { waitUntil: 'networkidle0' });
+  }
+
   await page.waitForSelector(selectors.timeRecording.recordButton);
   await screenshot(page, 'timerecording');
   await page.click(selectors.timeRecording.recordButton);
   consoleLog.write(`Time recorded => ${moment(Date.now()).format('hh:mm')}`, true, true);
   await screenshot(page, 'timerecorded');
+};
 
+const timeRecordingSheetValidation = async({ page, credentials }) => {
+  await page.waitFor(1500);
+  if (await page.$(selectors.timeRecording.modalRecordSheetValidation) !== null) {
+    consoleLog.write('Record Sheet validation...');
+    await page.focus(selectors.timeRecording.passwordInput);
+    await page.keyboard.type(credentials.password);
+    await page.click(selectors.timeRecording.confirmValidationButton);
+    await screenshot(page, 'recordsheet_validation');
+  }
+};
+
+const timeRecordingPageRedirect = async puppet => {
+  const page = puppet.page;
+  await page.goto(process.env.URL_TIMERECORDING, { waitUntil: 'networkidle0' });
+};
+
+module.exports = async puppet => {
+  await timeRecordingPageRedirect(puppet);
+  await timeRecordingSheetValidation(puppet);
+  await timeRecording(puppet);
   puppet.browser.close();
 };
