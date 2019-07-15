61ac82fc30e3aae4dcec3ae4ffd36f8c796d78bb
Renan Silveira <renanr.silveira@gmail.com>
2019-07-15 16:06:54 -0300

    Ajuste na criacao das pastas para armazenar os screenshots

--- helpers/screenshot.js
+++ helpers/screenshot.js
@@ -5,7 +5,10 @@ const scrDir = process.env.SCREENSHOT_DIR;
 module.exports = async(page, fileName) => {
   if (process.env.TAKE_SCREENSHOT) {
     const currentDate = new Date().toLocaleString();
-    const fullDir = scrDir.concat(`${currentDate.split(',')[0].replace(/\//g, '-').replace(/\s/g, '-')}/`);
+    const fullDir = scrDir.concat(`${currentDate.replace(/\//g, '-')
+      .replace(/,/g, '')
+      .split(' ')[0]}/`);
+
     if (!fs.existsSync(scrDir)) {
       fs.mkdirSync(scrDir);
     }
